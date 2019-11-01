b4f9d18bd400707d548b359bb08f8f19003170c2
Jeremy Reikes <jeremyreikes@gmail.com>
2019-11-01 18:58:33 -0400

    Fixed Topic Modeling.  Still needs to be transformed to normal dist

--- visualisations.py
+++ visualisations.py
@@ -2,20 +2,20 @@ import pandas as pd
 import spacy
 
 import scattertext as st
-df = pd.read_csv('train.csv')
-
-df.drop('id', axis=1, inplace=True)
-df.head()
+df = pd.read_csv('for_vis.csv')
+df = df.dropna()
+# df.drop('id', axis=1, inplace=Tru
+df.text.isna().sum()
 nlp = spacy.load('en')
-corpus = st.CorpusFromPandas(df,
+corpus = st.CorpusFromPandas(df.dropna(),
                              category_col='author',
                              text_col='text',
                              nlp=nlp).build()
 
 html = st.produce_scattertext_explorer(corpus,
-                                       category='EAP',
-                                       category_name='Edger Allen Poe',
-                                       not_category_name='HPL and MWS',
-                                       width_in_pixels=1000,
-                                       metadata=df['author'])
+                                       category='EAP')
 open("Author-Visualization.html", 'wb').write(html.encode('utf-8'))
+
+
+df.author
+df.text
