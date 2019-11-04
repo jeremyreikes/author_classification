eda2c61378e58b78386fe9c16747527eb4559729
Jeremy Reikes <jeremyreikes@gmail.com>
2019-11-03 20:28:24 -0500

    Organized and removed DRY code

--- spooky_authors.py
+++ spooky_authors.py
@@ -2,43 +2,49 @@ import pandas as pd
 import numpy as np
 import spacy
 from textblob import TextBlob
-import gensim
 import re
 from collections import Counter
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_regression
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn.metrics import accuracy_score
-import warnings
-from topic_modeling import get_topics
-warnings.filterwarnings("ignore")
-from tqdm import tqdm
-tqdm.pandas()
-
-df = pd.read_csv('train.csv')
-
-df.drop('id', axis=1, inplace=True)
-
-new_cols = ['entities', 'lemmas', 'raw_text_length', 'num_words', 'avg_word_len', 'vector_avg', 'polarity', 'subjectivity']
-false_cols = ['starts_conj', 'ends_prep', 'has_colon', 'has_semicolon', 'has_dash']
-for col in new_cols:
-    df[col] = np.nan
-for col in false_cols:
-    df[col] = np.zeros(shape=(len(df),))
-parts_of_speech = ['ADJ', 'ADV', 'ADP', 'AUX', 'CONJ', 'CCONJ', 'DET', 'EOL', 'NO_TAG', 'INTJ', 'NOUN', 'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT', 'SCONJ', 'SYM', 'VERB', 'X', 'SPACE']
-for pos in parts_of_speech:
-    df[pos] = np.zeros(shape=(len(df),)).astype('uint8')
-encoder = LabelEncoder()
-df['author'] = encoder.fit_transform(df.author.values)
+from topic_modeling import get_new_topic_probs, get_topic_probs
+import pronouncing
+import copy
+import textstat
 nlp = spacy.load("en_core_web_lg")
 
+def prepare_data():
+    '''Initializes DataFrame.'''
+    df = pd.read_csv('train.csv')
+    df.drop('id', axis=1, inplace=True)
+    new_cols = ['raw_text_length', 'num_words', 'avg_word_len', 'vector_avg', 'polarity', 'subjectivity', 'dale_chall', 'rhyme_frequency', 'FleischReadingEase', 'lexicon', 'word_diversity']
+    false_cols = ['starts_conj', 'ends_prep', 'has_colon', 'has_semicolon', 'has_dash', 'whom', 'has_had', 'num_ings']
+    empty_cols = ['entities', 'lemmas']
+    parts_of_speech = ['ADJ', 'ADV', 'ADP', 'AUX', 'CONJ', 'CCONJ', 'DET', 'EOL', 'NO_TAG', 'INTJ', 'NOUN', 'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT', 'SCONJ', 'SYM', 'VERB', 'X', 'SPACE']
+    for col in new_cols:
+        df[col] = np.zeros(shape=(len(df), ))
+    for col in empty_cols:
+        df[col] = np.empty(shape=(len(df), ))
+    for col in false_cols:
+        df[col] = np.zeros(shape=(len(df),))
+    for pos in parts_of_speech:
+        df[pos] = np.zeros(shape=(len(df),)).astype('uint8')
+    encoder = LabelEncoder()
+    df['author'] = encoder.fit_transform(df.author.values)
+    return df
+
+
+def rhyme_frequency(text):
+    words = list(set([i.lower() for i in text.split(' ')]))
+    num_words = len(words)
+    num_rhymes = 0
+    for i in words:
+        other = copy.copy(words)
+        other.remove(i)
+        rhymes = pronouncing.rhymes(i)
+        matches = set(other).intersection(set(rhymes))
+        num_rhymes += len(matches)/2
+    return num_rhymes/num_words
+
 def add_features(row):
+    '''Feature engineering via NLP.'''
     text = row.text
     doc = nlp(text)
     lemmas = list()
@@ -50,6 +56,12 @@ def add_features(row):
             row['has_semicolon'] = 1
         if token.text == '-':
             row['has_dash'] = 1
+        if token.text.lower() == 'whom':
+            row['whom'] = 1
+        if token.text[-3:] == 'ing':
+            row['num_ings'] += 1
+        if token.text.lower() == 'had':
+            row['has_had'] = 1
         pos = token.pos_
         row[pos] += 1
         if token.is_stop or not token.is_alpha:
@@ -71,67 +83,24 @@ def add_features(row):
     row['num_words'] = len(doc)
     row['avg_word_len'] = row.raw_text_length / row.num_words
     row['vector_avg'] = np.mean(nlp(lemmas).vector)
+    row['num_ings'] /= row['num_words']
+    row['rhyme_frequency'] = rhyme_frequency(row['text'])
+    row['dale_chall'] = textstat.dale_chall_readability_score(row['text'])
+    row['FleischReadingEase'] = textstat.flesch_reading_ease(row['text'])
+    row['lexicon'] = textstat.lexicon_count(row['text'])
+    row['word_diversity'] = row.lexicon/row.num_words
     return row
-df = df.apply(lambda x: add_features(x), axis=1)
-df['topic'] = get_topics(df.text.values.tolist(), num_topics = 6)
-dummies = pd.get_dummies(df.topic)
-df = pd.concat([df, dummies], axis=1)
 
-# df = pd.read_csv('dfwhatup.csv')
-remy = pd.read_csv('stacking.csv')
-remy.columns = [str(i) + '_x' for i in range(len(remy.columns))]
-remy_test = pd.read_csv('stacked_test.csv')
-remy_test.shape
-
-remy.shape
-remy_test.columns = [str(i) + '_x' for i in range(len(remy_test.columns))]
-X_train, X_valid, y_train, y_valid = train_test_split(df.drop(['author', 'lemmas', 'entities'], axis=1), df.author.values, test_size=0.25, random_state=0)
-
-tfidf = CountVectorizer()
-
-tfidf.fit(np.concatenate([X_train.text, X_valid.text]))
-X_train_tfidf = tfidf.transform(X_train.text)
-X_valid_tfidf = tfidf.transform(X_valid.text)
-
-x_train_array = pd.DataFrame(X_train_tfidf.toarray())
-x_valid_array = pd.DataFrame(X_valid_tfidf.toarray())
-
-X_train.reset_index(inplace=True)
-X_valid.reset_index(inplace=True)
-X_train_full = pd.concat([x_train_array, X_train], axis=1)
-X_valid_full = pd.concat([x_valid_array, X_valid], axis=1)
-X_train_final = X_train_full.drop(['text', 'index'], axis=1)
-X_valid_final = X_valid_full.drop(['text', 'index'], axis=1)
-
-# X_train_final['vector_avg'] = X_train_final['vector_avg'] - X_train_final.vector_avg.min()
-# X_valid_final['vector_avg'] = X_valid_final['vector_avg'] - X_valid_final.vector_avg.min()
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import svm, tree
-from xgboost import XGBClassifier
-
-
-
-for col in X_train_final.columns:
-    if X_train_final[col].values.min() < 0:
-        X_train_final[col] = X_train_final[col] - X_train_final[col].values.min()
-
-for col in X_valid_final.columns:
-    if X_valid_final[col].values.min() < 0:
-        X_valid_final[col] = X_valid_final[col] - X_valid_final[col].values.min()
-
-model = MultinomialNB(alpha=.1)
-model.fit(X_train_final, y_train)
-X_train_final.shape
-X_valid_final.shape
-# model = MultinomialNB(alpha=.1)
-# model.fit(X_train_final, y_train)
-preds = model.predict(X_valid_final)
-acc = accuracy_score(y_valid, preds)
-print('TFIDF Accuracy: ', acc)
-
-
-
-X_train_final.iloc[1, -35:]
-
-X_train_final.to_csv('X_train.csv')
-X_valid_final.to_csv('X_valid.csv')
+def process_training_data():
+    '''Engineers features and performs train test split.'''
+    data = prepare_data()
+    df = data.apply(lambda x: add_features(x), axis=1)
+    df['vector_avg'] = df['vector_avg'] - df['vector_avg'].min()
+    topic_probs, lda_model = get_topic_probs(df)
+    df = pd.concat([df, topic_probs], axis=1)
+    cv = CountVectorizer()
+    cv.fit(df.text)
+    cv_transformed = cv.transform(df.text).toarray()
+    full_df = pd.concat([df, pd.DataFrame(cv_transformed)], axis=1)
+    processed_data = train_test_split(full_df.drop(['author', 'lemmas', 'entities'], axis=1), full_df.author.values, test_size=0.1, random_state=0)
+    return processed_data # in form X_train, X_val, y_train, y_val
